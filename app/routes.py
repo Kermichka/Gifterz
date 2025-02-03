bed6a9689fdcbc3b6c0fbc1f3bff62a7a180ed69
Kermichka <anastacia.zlatinova@gmail.com>
2025-02-03 14:42:36 +0200

    divide get and post and add more sqlalchemy

--- app/routes.py
+++ app/routes.py
@@ -16,11 +16,20 @@ from sqlalchemy.orm import joinedload
 main = Blueprint("main", __name__)
 
 
-@main.route("/", methods=["GET", "POST"])
+@main.route("/", methods=["GET"])
 def landing_page():
+    """Render the landing page with both login and register forms."""
     login_form = LoginForm()
     register_form = RegisterForm()
+    return render_template(
+        "landing.html", login_form=login_form, register_form=register_form
+    )
 
+
+@main.route("/login", methods=["POST"])
+def login():
+    """Handle user login separately."""
+    login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user and user.check_password(login_form.password.data):
@@ -29,18 +38,27 @@ def landing_page():
             return redirect(url_for("main.profile"))
         else:
             flash("Invalid email or password.", "danger")
-    # Make 2 seperate endpoints/elifs
+    return redirect(url_for("main.landing_page"))
+
+
+@main.route("/register", methods=["POST"])
+def register():
+    """Handle user registration separately."""
+    register_form = RegisterForm()
     if register_form.validate_on_submit():
         existing_user = User.query.filter_by(email=register_form.email.data).first()
         if existing_user:
             flash("Email is already in use. Please log in.", "warning")
             return redirect(url_for("main.landing_page"))
 
+        # Create new user and hash the password
         new_user = User(
             name=register_form.name.data,
             email=register_form.email.data,
         )
-        new_user.password = register_form.password.data
+        new_user.password = (
+            register_form.password.data
+        )  # Uses property setter to hash password
         db.session.add(new_user)
         db.session.commit()
 
@@ -48,9 +66,7 @@ def landing_page():
         flash("Registration successful!", "success")
         return redirect(url_for("main.profile"))
 
-    return render_template(
-        "landing.html", login_form=login_form, register_form=register_form
-    )
+    return redirect(url_for("main.landing_page"))
 
 
 @main.route("/profile")
@@ -82,11 +98,13 @@ def profile():
 def view_wishlist_details(list_id):
     wishlist = List.query.get_or_404(list_id)
     form = GiftForm()
-    gifts = Present.query.filter_by(list_id=wishlist.id).all()
 
-    # TODO: with join! no forloops to obtain database data
-    for gift in gifts:
-        gift.group = Group.query.filter_by(present_id=gift.id).first()
+    gifts = (
+        db.session.query(Present, Group)
+        .join(Group, Group.present_id == Present.id, isouter=True)
+        .filter(Present.list_id == wishlist.id)
+        .all()
+    )
 
     return render_template("wishlist.html", wishlist=wishlist, form=form, gifts=gifts)
 
@@ -151,10 +169,9 @@ def search_users():
     return render_template("search_results.html", query=query, users=users)
 
 
-@main.route("/user/<int:user_id>/wishlist/<int:list_id>", methods=["GET", "POST"])
+@main.route("/user/<int:user_id>/wishlist/<int:list_id>", methods=["GET"])
 @login_required
-# Divide 2, for get and post
-def view_user_wishlist_details(user_id, list_id):
+def view_user_wishlist_get(user_id, list_id):
     user = User.query.get_or_404(user_id)
     wishlist = List.query.get_or_404(list_id)
     presents = Present.query.filter_by(list_id=wishlist.id).all()
@@ -167,6 +184,22 @@ def view_user_wishlist_details(user_id, list_id):
         else:
             present.reserved = False
 
+    return render_template(
+        "wishlist_details.html",
+        user=user,
+        wishlist=wishlist,
+        presents=presents,
+        reserve_form=reserve_form,
+    )
+
+
+@main.route("/user/<int:user_id>/wishlist/<int:list_id>", methods=["POST"])
+@login_required
+def view_user_wishlist_post(user_id, list_id):
+    user = User.query.get_or_404(user_id)
+    wishlist = List.query.get_or_404(list_id)
+    reserve_form = ReserveForm()
+
     if reserve_form.validate_on_submit():
         present_id = request.form.get("present_id")
         present = Present.query.get_or_404(present_id)
@@ -175,7 +208,7 @@ def view_user_wishlist_details(user_id, list_id):
             flash("This present has already been reserved.", "warning")
             return redirect(
                 url_for(
-                    "main.view_user_wishlist_details",
+                    "main.view_user_wishlist_get",
                     user_id=user.id,
                     list_id=wishlist.id,
                 )
@@ -194,10 +227,15 @@ def view_user_wishlist_details(user_id, list_id):
         db.session.commit()
 
         return redirect(
-            url_for(
-                "main.view_user_wishlist_details", user_id=user.id, list_id=wishlist.id
-            )
+            url_for("main.view_user_wishlist_get", user_id=user.id, list_id=wishlist.id)
         )
+
+    presents = Present.query.filter_by(list_id=wishlist.id).all()
+    for present in presents:
+        if present.group_id:
+            present.reserved = True
+        else:
+            present.reserved = False
 
     return render_template(
         "wishlist_details.html",
@@ -208,73 +246,17 @@ def view_user_wishlist_details(user_id, list_id):
     )
 
 
-@main.route("/group/<int:group_id>/join", methods=["POST"])
-@login_required
-def join_group(group_id):
-    group = Group.query.get_or_404(group_id)
-    present = Present.query.get_or_404(group.present_id)
-
-    existing_reservation = Buyer.query.filter_by(
-        user_id=current_user.id, group_id=group_id
-    ).first()
-
-    if existing_reservation:
-        flash("You have already reserved this present in this group.", "warning")
-        return redirect(url_for("main.profile"))
-
-    current_group_size = Buyer.query.filter_by(group_id=group_id).count()
-    if current_group_size >= group.group_size:
-        flash("Sorry, this group is already full.", "warning")
-        return redirect(url_for("main.profile"))
-
-    new_reservation = Buyer(
-        user_id=current_user.id, group_id=group_id, contribution_amount=0.0
-    )
-    db.session.add(new_reservation)
-
-    present.reserved_by_id = current_user.id
-    db.session.commit()
-
-    flash("You have successfully joined the group and reserved the present!", "success")
-    return redirect(url_for("main.profile"))
-
-
-@main.route("/group/<int:group_id>")
-@login_required
-def view_group(group_id):
-    group = Group.query.get_or_404(group_id)
-    present = Present.query.get_or_404(group.present_id)
-
-    group_members = Buyer.query.filter_by(group_id=group_id).all()
-
-    reserved_by_user = None
-    if present.reserved_by_id:
-        reserved_by_user = User.query.get(present.reserved_by_id)
-
-    return render_template(
-        "group_detail.html",
-        group=group,
-        present=present,
-        group_members=group_members,
-        reserved_by_user=reserved_by_user,
-    )
-
-
 @main.route("/create_group/<int:present_id>", methods=["POST"])
 @login_required
 def create_group(present_id):
-    # Get the present by ID
     present = Present.query.get_or_404(present_id)
 
-    # Check if the present already has a group
     if present.group_id:
         flash("This present already has a group.", "warning")
         return redirect(url_for("main.view_user_wishlist", user_id=current_user.id))
 
-    # Get the group size from the form
     group_size = request.form.get("group_size", type=int)
 
-    # Create a new group for the present
     new_group = Group(present_id=present.id, group_size=group_size)
     db.session.add(new_group)
     db.session.commit()
